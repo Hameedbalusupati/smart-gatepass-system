3a68b665a1883d6e47f13118f0823ff763b548f1
Hameedbalusupati <hameedbalusuapti023@gmail.com>
2026-04-15 21:50:23 +0530

    fixed api import path issue

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import API from "../api.js";
+import API from "../api/api.js";
 import "../index.css";
 
 export default function Login() {
